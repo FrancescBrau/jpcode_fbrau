e473c5ea7e787fec4e3e22e6f4c05d5bae15b47c
Francesc Brau <frjosepgbrau@gmail.com>
2024-10-14 11:26:06 +0400

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const JpApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class JpApp extends StatelessWidget {
+  const JpApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Hello JP!'),
         ),
       ),
     );
